24d4d1336265466c1a20c445798673525f343d09
DanSnow <dododavid006@gmail.com>
2022-04-24 00:13:14 +0800

    feat: read runtime update url

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod transform_sites;
 
 use anyhow::Result;
 use indoc::indoc;
-use std::time::SystemTime;
+use std::{env, time::SystemTime};
 use tap::prelude::*;
 use time::{macros::format_description as fd, OffsetDateTime};
 use transform_sites::transform_sites;
@@ -11,7 +11,10 @@ static CONTENT_FARM: &str =
     "https://danny0838.github.io/content-farm-terminator/files/blocklist/content-farms.txt";
 
 fn main() -> Result<()> {
-    let update_url = option_env!("UPDATE_URL").unwrap_or("");
+    let default_update_url = option_env!("UPDATE_URL").unwrap_or("");
+    let runtime_update_url = env::args().nth(1).or_else(|| env::var("UPDATE_URL").ok());
+    let update_url = runtime_update_url.as_deref().unwrap_or(default_update_url);
+
     let content = ureq::get(CONTENT_FARM).call()?.into_string()?;
     println!("! Title: zh Content Farms List");
     if !update_url.is_empty() {
